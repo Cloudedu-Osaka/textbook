76299497145f232d3a9f4eef7612e9fbc6e49d49
moyhig-ecs <u416528d@ecs.osaka-u.ac.jp>
2022-05-10 13:24:24 +0900

    .

--- _build/jupyter_execute/chapters/04/1/Numbers.py
+++ _build/jupyter_execute/chapters/04/1/Numbers.py
@@ -1,72 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
-# initialization for my classroom
-import os
-from datetime import datetime as dt
-
-def logfile(user=os.environ.get('JUPYTERHUB_USER') or 'jovyan'):
-    prefix='/srv'
-    if os.path.isdir(prefix) and os.access(prefix, os.W_OK):
-        prefix+=('/'+user)
-        if not os.path.isdir(prefix):
-            os.makedirs(prefix)
-    else:
-        prefix='.'
-    return prefix+'/'+dt.now().strftime('%Y%m%d')+'.log'
-
-path=logfile()
-#%logstop
-get_ipython().run_line_magic('logstart', '-otq $path append')
-
-# [python - cannot override sys.excepthook - Stack Overflow](https://stackoverflow.com/questions/1261668/cannot-override-sys-excepthook/28758396)
-# https://github.com/ipython/ipython/blob/e6432249582e05f438303ce73d082a0351bb383e/IPython/core/interactiveshell.py#L1952
-
-import sys
-import traceback
-import IPython
-
-try:
-    _showtraceback
-except NameError:
-    _showtraceback=IPython.core.interactiveshell.InteractiveShell.showtraceback
-
-try:
-    _showsyntaxerror
-except NameError:
-    _showsyntaxerror=IPython.core.interactiveshell.InteractiveShell.showsyntaxerror
-
-import logging
-logging.basicConfig(filename=path.replace('.log','-exc.log'), format='%(asctime)s %(message)s', level=logging.ERROR, force=True)
-
-import sys
-import traceback
-import IPython
-
-def showtraceback(self, *args, **kwargs):
-    etype, value, tb = self._get_exc_info(kwargs.get('exc_tuple'))
-    stb = self.InteractiveTB.structured_traceback(
-        etype, value, tb, tb_offset=kwargs.get('tb_offset'))
-    logging.error(os.environ.get('JUPYTERHUB_USER') or 'jovyan')
-    logging.error(self.InteractiveTB.stb2text(stb))
-    _showtraceback(self, *args, **kwargs)
-
-def showsyntaxerror(self, *args, **kwargs):
-    etype, value, last_traceback = self._get_exc_info()
-    elist = traceback.extract_tb(last_traceback) if kwargs.get('running_compiled_code') else []
-    stb = self.SyntaxTB.structured_traceback(etype, value, elist)
-    logging.error(os.environ.get('JUPYTERHUB_USER') or 'jovyan')
-    logging.error(self.InteractiveTB.stb2text(stb))
-    _showsyntaxerror(self, *args, **kwargs)
-
-IPython.core.interactiveshell.InteractiveShell.showtraceback = showtraceback
-IPython.core.interactiveshell.InteractiveShell.showsyntaxerror = showsyntaxerror
-
-
-# In[ ]:
+# In[3]:
 
 
 # initialization for my classroom
